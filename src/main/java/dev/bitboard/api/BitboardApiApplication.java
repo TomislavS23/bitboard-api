db0557c1a2dbf053fac1c9e7fbee9969f0f94bf4
TomislavS23 <senjanin.tomislav@gmail.com>
2024-11-19 23:23:08 +0100

    initial setup of Spring Boot REST API

---
+++ src/main/java/dev/bitboard/api/BitboardApiApplication.java
@@ -0,0 +1,13 @@
+package dev.bitboard.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitboardApiApplication.class, args);
+    }
+
+}
